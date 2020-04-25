8b99369024ec738405699e3bcf9fa8aad01eeff7
marat <m.gaynutdinov@mail.ru>
2020-04-25 21:03:05 +0300

    Completed the 3th test

--- vehicle.go
+++ vehicle.go
@@ -146,3 +146,32 @@ func generateRating() {
 		vehicleResult[v.Name] = vehResult
 	}
 }
+
+func showRating(model string) {
+    ratingFound := false
+    for m,r := range vehicleResult{
+	    if m == model {
+			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v", r.feedbackTotal, r.feedbackPositive, r.feedbackNegative, r.feedbackNeutral)
+			ratingFound = true
+		}
+	}
+    if !ratingFound {
+	    fmt.Printf("No rating for this vehicle")
+	}
+}
+
+func (c *car) carDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Car" , c.make, c.model)
+	showRating(c.model)
+}
+
+func (b *bike) bikeDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Bike", b.make, b.model)
+	showRating(b.model)
+}
+
+func (t *truck) truckDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Truck", t.make, t.model)
+	showRating(t.model)
+}
+
